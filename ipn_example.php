9c394116daf817a5376b734652cfbe545c70cbc5
Md Khaled Hossain <37791345+khaled3139@users.noreply.github.com>
2020-08-05 22:19:59 +0600

    Create ipn_example.php
    IPN Example Added

---
+++ ipn_example.php
@@ -0,0 +1,37 @@
+<?php 
+
+// Fill these in with the information from your WalletCore account. 
+$ipn_secret = "TESTIPNSECRET"; // Same as WalletCore
+$raw_data = file_get_contents("php://input"); // Post Josn Data
+$request = json_decode($raw_data,true); // Decode Json Data
+
+
+if ($request === FALSE || empty($request)) { 
+    die("Data not Found!");
+    exit();
+} 
+
+    // Data form WalletCore
+    $address        = $request['address'];
+    $recvied        = $request['amount'];
+    $tnxid          = $request['tnxid']; 
+    $IPNSECRET      = $request['IPNSECRET']; 
+    $confirmations  = $request['confirmations']; 
+    
+
+    if ($confirmations < 3) {    
+        die("Need Minimum 3 Confirmation");
+        exit();
+    }
+
+    if ($IPNSECRET == IPNSECRET) {
+
+        // Here do your code as you want now transction are valid and successfuly get 3 Confirmaton
+
+
+
+    }else{
+        die("IPN Secret Not Match");
+        exit();
+    } 
+?>
